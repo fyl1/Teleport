459b022e80585a1e00cdad24dbc624b1bfaa9251
fyl1 <38299430+fyl1@users.noreply.github.com>
2020-11-25 08:14:40 +0200

    add file

--- js/script.js
+++ js/script.js
@@ -6,3 +6,33 @@ var msnry = new Masonry(senseList, {
   percentPosition: true,
   gutter: 20,
 });
+const acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+const menuToggle = document.querySelector("#menu__toggle");
+const headerInner = document.querySelector(".header__inner");
+const headerLogo = document.querySelector(".header__logo");
+const headerBtn = document.querySelector(".header-btn");
+
+menuToggle.addEventListener("change", function () {
+  if (this.checked) {
+    headerLogo.classList.add("active-logo");
+    headerInner.classList.add("center");
+    headerBtn.classList.add("hidden");
+  } else {
+    headerLogo.classList.remove("active-logo");
+    headerInner.classList.remove("center");
+    headerBtn.classList.remove("hidden");
+  }
+});
